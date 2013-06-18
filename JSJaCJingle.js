fdd64f7a744440ecd5f819ef2d502eb51a5962b9
Valérian Saliou <val.saliou@gmail.com>
2013-06-18 17:32:32 +0200

    Added example workflow for more understanding

--- JSJaCJingle.js
+++ JSJaCJingle.js
@@ -2,9 +2,31 @@
  * @fileoverview JSJaC Jingle library, implementation of XEP-0166.
  * Built originally for Uno.im service requirements
  *
- * @url http://xmpp.org/extensions/xep-0166.html
  * @author Valérian Saliou valerian@jappix.com
  * @license Mozilla Public License (MPL)
+ */
+
+
+/**
+ * Workflow
+ *
+ * Implements: XEP-0166
+ * URL: http://xmpp.org/extensions/xep-0166.html
+
+ * 1.cmt Local user wants to start a WebRTC session with remote user
+ * 1.snd Local user sends a session-initiate type='set'
+ * 1.hdl Remote user sends back a type='result' to '1.snd' stanza (ack)
+
+ * 2.cmt Local user waits silently for remote user to send a session-accept
+ * 2.hdl Remote user sends a session-accept type='set'
+ * 2.snd Local user sends back a type='result' to '2.hdl' stanza (ack)
+
+ * 3.cmt WebRTC session starts
+ * 3.cmt Users chat, and chat, and chat. Happy Jabbering to them!
+
+ * 4.cmt Local user wants to stop WebRTC session with remote user
+ * 4.snd Local user sends a session-terminate type='set'
+ * 4.hdl Remote user sends back a type='result' to '4.snd' stanza (ack)
  */
 
 
