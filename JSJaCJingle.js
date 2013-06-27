0399c57789055e9ca56f7843a2fb2e9d171aec49
Valérian Saliou <val.saliou@gmail.com>
2013-06-27 14:15:38 +0200

    fixes #9 - Initial v0.1 release

--- JSJaCJingle.js
+++ JSJaCJingle.js
@@ -2,6 +2,7 @@
  * @fileoverview JSJaC Jingle library, implementation of XEP-0166.
  * Written originally for Uno.im service requirements
  *
+ * @version v0.1
  * @url https://github.com/valeriansaliou/jsjac-jingle
  * @depends https://github.com/sstrigler/JSJaC
  * @author Valérian Saliou valerian@jappix.com
