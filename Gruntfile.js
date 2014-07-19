d03011551a0100181bafac894421c3d6e8fa98b0
Valérian Saliou <valerian@valeriansaliou.name>
2014-07-19 13:50:55 +0200

    v0.7.1 release

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,7 @@ module.exports = function(grunt) {
   // Map tasks
   var GRUNT_TASKS_BUILD = {
     main: ['clean:reset', 'bower:install', 'concat', 'copy', 'uglify', 'jsdoc', 'clean:temporary'],
+    quick: ['clean:reset', 'bower:install', 'concat', 'copy', 'clean:temporary'],
     dev: ['clean:temporary', 'bower:install', 'concat', 'copy', 'clean:temporary'],
     test: ['clean:temporary', 'bower:install', 'concat', 'copy', 'clean:temporary']
   };
